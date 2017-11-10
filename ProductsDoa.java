21db375cf9d79df1a3cf0d9f91b26c5563bb7ec6
oldwin4 <33552513+oldwin4@users.noreply.github.com>
2017-11-10 20:32:57 +0300

    Create ProductsDoa.java

---
+++ ProductsDoa.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import java.io.IOException;
+
+public interface ProductsDoa {
+    boolean exists(String product) throws IOException;
+
+    void add(Product product) throws IOException;
+
+    int getprice(String productName) throws IOException;
+
+    void remove(String product) throws IOException;
+
+    void replace(String product1, String product2) throws IOException;
+
+}
