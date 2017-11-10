ca5b7fcc22354127c82284136c8971dab59c94d2
oldwin4 <33552513+oldwin4@users.noreply.github.com>
2017-11-10 20:30:07 +0300

    Create main.java

---
+++ main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException, ProductNotFoundException {
+        File file = new File("products.csv");
+        ProductsDao dao = new ProductsDaoCsvImplementation();
+        System.out.println(dao.getprice("яблоки"));
+
+        //Product product = new Product("хлеб", 40, "Хлебзавод1");
+        //dao.add(product);
+
+        System.out.println(dao.exists("печенье"));
+    }
+}
